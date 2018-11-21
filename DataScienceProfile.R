25c01a2aa39c9f3e56e3a6692c8401843c485e02
Joe Jiang <yanliu3936@gmail.com>
2018-11-21 16:48:10 -0600

    Add my data science profile

---
+++ DataScienceProfile.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(dplyr)
+joe <- data.frame(categories = c("computer programming", "math", "statistics", "machine learning", 
+                                 "domain expertise", "communication and presentation skills", "data visualization"), 
+                  answer = c(1,2,2,0,5,3,2))
+joe%>%ggplot(aes(categories, answer))+
+  geom_bar(stat = "identity")+
+  theme(axis.text.x = element_text(angle = 75, hjust = 1))
+ggsave("Figs/Bargraph.png")
