c195eb02d624dcd275b09d424b3a0eea3c6413b5
JoeJiang3936git config --global user.na[Ame ''JoeJiang3936 <yanliu3936@gmail.com>
2018-11-21 17:26:46 -0600

    Add the readme file

--- DataScienceProfile.R
+++ DataScienceProfile.R
@@ -5,5 +5,4 @@ joe <- data.frame(categories = c("computer programming", "math", "statistics", "
                   answer = c(1,2,2,0,5,3,2))
 joe%>%ggplot(aes(categories, answer))+
   geom_bar(stat = "identity")+
-  theme(axis.text.x = element_text(angle = 75, hjust = 1))
-ggsave("Figs/Bargraph.png")
+  theme(axis.text.x = element_text(angle = 75, hjust = 1))hkj
---
+++ README.md
@@ -0,0 +1,2 @@
+In this repo, I added data science profile which contains a plot (MyProfile.pdf) and the R code (DataScienceProfile.R) used to generate the plot
+I also added files from Monnie McGee's stat6306datascience repo
